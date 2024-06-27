cba9f251da41f307210803fc6d7f39c25867a2e7
Miras <iskakov91633@gmail.com>
2024-06-27 23:42:23 +0500

    Added AuthViewController

--- MusicBliss/Controllers/Core/TabBarViewController.swift
+++ MusicBliss/Controllers/Core/TabBarViewController.swift
@@ -30,7 +30,7 @@ class TabBarViewController: UITabBarController {
         
         nav1.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house"), tag: 1)
         nav2.tabBarItem = UITabBarItem(title: "Search", image: UIImage(systemName: "magnifyingglass"), tag: 2)
-        nav3.tabBarItem = UITabBarItem(title: "Library", image: UIImage(systemName: "building.columns"), tag: 3)
+        nav3.tabBarItem = UITabBarItem(title: "Library", image: UIImage(systemName: "music.note.list"), tag: 3)
 
         
         nav1.navigationBar.prefersLargeTitles = true
--- MusicBliss/Managers/AuthManager.swift
+++ MusicBliss/Managers/AuthManager.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+final class AuthManager {
+    static let shared = AuthManager()
+    
+    struct Constants {
+        static let clientID = "873a51705c544578bdb474bb7157792b"
+        static let clientSecret = "0bdc30a6cee1466d89038fdb5d401207"
+    }
+    
+    private init() {}
+    
+    var isSingedIn: Bool {
+        return false
+    }
+    
+    private var accessToken: String? {
+        return nil
+    }
+    
+    private var refreshToken: String? {
+        return nil
+    }
+    
+    private var tokenExpirationDate: Date? {
+        return nil
+    }
+    
+    private var shouldRefreshToken: Bool {
+        return false
+    }
+}
